83c5111caf9c41be3a1ffca349a7417f3b6c78fe
devanfer <ferrelvsc20@gmail.com>
2025-11-18 00:24:29 +0700

    chore: add opengraph metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,28 @@ export const metadata: Metadata = {
   description: "Rate My University Brawijaya Professors",
   icons: {
     icon: "https://wiki.ub.ac.id/lib/exe/fetch.php?cache=&media=ub-logo-small.png"
+  },
+  openGraph: {
+    title: "RateMyUbProf",
+    description: "Rate My University Brawijaya Professors",
+    url: "https://rmubprof.dvnnfrr.my.id",
+    siteName: "Rate My UB professor",
+    images: [
+      {
+        url: "https://wiki.ub.ac.id/lib/exe/fetch.php?cache=&media=ub-logo-small.png",
+        width: 1200,
+        height: 630,
+        alt: "UB Logo"
+      }
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "RateMyUbProf",
+    description: "Rate My University Brawijaya Professors",
+    images: ["https://wiki.ub.ac.id/lib/exe/fetch.php?cache=&media=ub-logo-small.png"]
   }
 };
 
